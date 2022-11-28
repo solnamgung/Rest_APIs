517bd09e732b946db378471c0440e7f2603464f8
sol namgung <nandaras0103@gmail.com>
2022-11-28 21:01:59 +0900

    test

--- udacity-java-master/Lesson2-restapis/bootstrap/src/main/java/com/udacity/bootstrap/BootstrapApplication.java
+++ udacity-java-master/Lesson2-restapis/bootstrap/src/main/java/com/udacity/bootstrap/BootstrapApplication.java
@@ -2,7 +2,7 @@ package com.udacity.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test
 @SpringBootApplication
 public class BootstrapApplication {
 
